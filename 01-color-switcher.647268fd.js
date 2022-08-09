bd5d4396c1d3a44ea00565815c90cee9aa9fa293
ZadAndRot <96700276+ZadAndRot@users.noreply.github.com>
2022-08-09 15:27:09 +0000

    Deploying to gh-pages from @ ZadAndRot/goit-js-hw-09@07175a0c348f6aca0c9e04674c0bf1efa6665d0e 🚀

---
+++ 01-color-switcher.647268fd.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;function d(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}e.disabled=!0,t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,o=setInterval((function(){d()}),1e3),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(o)}))}))}();
+//# sourceMappingURL=01-color-switcher.647268fd.js.map
